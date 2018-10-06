83a3a259307f59e70a52c70d8d5e53993625812f
David Gurley <davidmgurley@gmail.com>
2018-10-06 14:41:51 -0600

    initial budget setup page styling adjusted

--- skybudget1/android/app/src/components/GettingStarted.js
+++ skybudget1/android/app/src/components/GettingStarted.js
@@ -148,16 +148,16 @@ saveData = () => {
         <View style={styles.container}>
           <Image style={styles.rocket} source={require('./rocket.png')} />
 
-          <Text style={styles.instructions}>How much would you like to spend a month?</Text>
+          <Text style={styles.instructions}>How much would you like to spend each month?</Text>
           <View style={styles.dollarInputView}>
-            <Text style={styles.dollarSign}>$</Text>
-            <TextInput onChangeText={totalMonthlySpending => this.setState({totalMonthlySpending})} style={styles.dollarInput} keyboardType='numeric' placeholder='0'></TextInput>
+            <Text style={styles.dollarSignInitial}>$</Text>
+            <TextInput onChangeText={totalMonthlySpending => this.setState({totalMonthlySpending})} style={styles.dollarInputInitial} keyboardType='numeric' placeholder='0'></TextInput>
           </View>
           <TouchableOpacity  onPress={this.showCreateBudget}>
             <Text style={styles.startButton}>Submit</Text>
           </TouchableOpacity>
           <View style={styles.toolTip}>
-            <Text style={styles.toolTipText}>Budget Tip!</Text>
+            <Text style={styles.toolTipTextTitle}>Budget Tip!</Text>
             <Text style={styles.toolTipText}>We recommend putting your entire monthly income as your monthly spending budget. Every healthy budget has plenty of space for savings and investment. You will account for those dollars when setting up your bills section in the next step.</Text>
           </View>
         </View> }
@@ -171,150 +171,183 @@ saveData = () => {
 
 const styles = EStyleSheet.create({
   individualBudgetInputContainer: {
-    fontSize: 20,
+    fontSize: '20rem',
     color: 'white',
     justifyContent: 'space-between',
     fontFamily: 'Lato-Regular'
-
   },
 
   rocket: {
-    width: '75rem',
-    height: '100rem'
+    width: '65rem',
+    height: '90rem',
+    marginBottom: '10rem'
   },
+
   dollarSignBlue: {
-    fontSize:30,
+    fontSize:'25rem',
     color: 'white',
-    paddingRight: 5,
-    paddingLeft: 5,
+    paddingRight: '7rem',
+    paddingLeft: '7rem',
+    paddingTop: '3rem',
+    paddingBottom: '3rem',
     backgroundColor: '#2A6972',
     borderRadius: 5,
+    marginBottom: '5rem',
     fontFamily: 'Lato-Regular'
-
   },
+
   dollarSignRed: {
-    fontSize:30,
+    fontSize:'25rem',
     color: 'white',
-    paddingRight: 5,
-    paddingLeft: 5,
+    paddingRight: '7rem',
+    paddingLeft: '7rem',
+    paddingTop: '3rem',
+    paddingBottom: '3rem',
     backgroundColor: '#C95B74',
     borderRadius: 5,
+    marginBottom: '5rem',
     fontFamily: 'Lato-Regular'
-
   },
+
   dollarSignGreen: {
-    fontSize:30,
+    fontSize:'25rem',
     color: 'white',
-    paddingRight: 5,
-    paddingLeft: 5,
+    paddingRight: '7rem',
+    paddingLeft: '7rem',
+    paddingTop: '3rem',
+    paddingBottom: '3rem',
     backgroundColor: '#5D9D83',
     borderRadius: 5,
+    marginBottom: '5rem',
     fontFamily: 'Lato-Regular'
-
   },
-  dollarSign: {
-    fontSize:30,
+
+  dollarSignInitial: {
+    fontSize:'20rem',
     color: 'white',
-    marginBottom: 3,
+    marginBottom: 10,
     fontFamily: 'Lato-Regular'
-
-
   },
+
   toolTip: {
-    height: 190,
-    width: 350,
+    height: '125rem',
+    width: '275rem',
     backgroundColor: '#EAAF69',
-    marginTop: 50,
+    marginTop: '40rem',
     justifyContent: 'center',
     borderRadius: 20
   },
+
   toolTipText: {
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 5,
-    marginBottom: 8,
-    fontSize: 16,
+    marginLeft: '10rem',
+    marginRight: '10rem',
+    marginTop: '5rem',
+    marginBottom: '5rem',
+    fontSize: '12rem',
     fontFamily: 'Lato-Regular'
-
-
   },
+
+  toolTipTextTitle: {
+    textAlign: 'center',
+    marginTop: '5rem',
+    marginBottom: '5rem',
+    fontSize: '15rem',
+    fontFamily: 'Lato-Regular'
+  },
+
   dollarInput:{
-    width: 200,
+    width: '165rem',
     backgroundColor: 'white',
-    fontSize: 20,
-    marginLeft: 10,
-    marginBottom: 10,
+    fontSize: '16rem',
+    marginLeft: '20rem',
+    marginBottom: '10rem',
     borderRadius: 5,
     fontFamily: 'Lato-Regular'
-
   },
+
+  dollarInputInitial:{
+    width: '100rem',
+    height: '35rem',
+    backgroundColor: 'white',
+    fontSize: '15rem',
+    marginLeft: 5,
+    marginRight: '7rem',
+    marginBottom: '5rem',
+    borderRadius: 5,
+    fontFamily: 'Lato-Regular'
+  },
+
   dollarInputView:{
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between'
   },
+
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#1B2F4A',
   },
+
   welcome: {
     fontSize: 50,
     textAlign: 'center',
     margin: 10,
     color: '#2A6972',
     fontFamily: 'Lato-Regular'
-
   },
+
   instructions: {
-    fontSize: '22rem',
+    fontSize: '23rem',
     textAlign: 'center',
     color: '#2A6972',
-    marginBottom: 40,
+    marginBottom: '20rem',
+    marginLeft: '20rem',
+    marginRight: '20rem',
     fontFamily: 'Lato-Regular'
-
   },
+
   instructions2: {
-    fontSize: 25,
+    fontSize: '20rem',
     textAlign: 'center',
     color: '#2A6972',
     marginBottom: 20,
     fontFamily: 'Lato-Regular'
-
   },
+
   startButton: {
     color: '#2A6972',
-    fontSize: 30,
+    fontSize: '25rem',
     marginTop: 10,
     textAlign: 'center',
     fontFamily: 'Lato-Regular'
-
   },
+
   startOverButton: {
     color: 'white',
-    fontSize: 30,
-    marginTop: 10,
+    fontSize: '25rem',
+    marginTop: '8rem',
     textAlign: 'center',
     backgroundColor: '#C95B74',
-    paddingRight: 10,
-    paddingLeft: 10,
+    paddingRight: '10rem',
+    paddingLeft: '10rem',
     borderRadius: 5,
     fontFamily: 'Lato-Regular'
-
   },
+
   startSubmitButton: {
     color: 'white',
-    fontSize: 30,
-    marginTop: 10,
+    fontSize: '25rem',
+    marginTop: '8rem',
     textAlign: 'center',
     backgroundColor: '#5D9D83',
-    paddingRight: 10,
-    paddingLeft: 10,
+    paddingRight: '10rem',
+    paddingLeft: '10rem',
     borderRadius: 5,
     fontFamily: 'Lato-Regular'
-
   },
+
   twoButtonGroup: {
     flexDirection: 'row',
     justifyContent: 'space-between',
